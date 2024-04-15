56c18b92a9c9109e5e21e87fed2c9f8602fc5ad2
ssohuiKim <soooooh7@gmail.com>
2024-04-15 23:26:50 +0900

    navbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0q!n&-ur&2m2a48s$9-_*^t+4jz$@^9tm!kdj^c5wt0g(%%6m4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
